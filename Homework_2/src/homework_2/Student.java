b46a653f859c03c1e3df6f680b6476e454d8521b
KhuramC <khuramchoudhry123@gmail.com>
2024-02-04 13:52:40 -0600

    Completion of Student.java requirements
    Implemented ID/name/grade as private fields with associated
    getter/setters. Default and parameterized constructors were created
    along with a toString.

--- Homework_2/src/homework_2/Student.java
+++ Homework_2/src/homework_2/Student.java
@@ -1,5 +1,43 @@
 package homework_2;
 
 public class Student {
+		
+	private int id;
+	private String name;
+	private double grade;
+	
+	public Student() {
+		this.id = 0;
+		this.name = "";
+		this.grade = 0;
+	}
+	public Student(int id, String name, double grade) {
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+	public String toString() {
+		String s = this.name + " has an ID of " + this.id + " and has a grade of " + this.grade + ".";
+		return s;
+	}
 
 }
