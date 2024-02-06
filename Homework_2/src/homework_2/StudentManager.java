d72f238b234339b96c70de4d6d1bc91f0e3cde21
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-02-06 13:16:10 -0600

    Added to readFromFile
    uses a list and converts it to an array to be stored in the students array.

--- Homework_2/src/homework_2/StudentManager.java
+++ Homework_2/src/homework_2/StudentManager.java
@@ -1,20 +1,56 @@
 package homework_2;
 
+import java.io.FileInputStream;
+import java.util.*;
+
 public class StudentManager {
 	
 	private Student students[];
-	
-
-	
 	
 	public static void main(String[] args) {
 		StudentManager sd = new StudentManager();
 		sd.students[0] = new Student();
 		
 	}
-	
+
+
 	//Method must have this prototype from the doc
 	public boolean readFromFile(String fileName) {
+		try{
+			FileInputStream input = new FileInputStream("files/"+fileName);
+			Scanner scanner = new Scanner(input);
+			
+			List<Student> studentList = new ArrayList<>();
+			
+			//Read student data to initialize Student objects
+			while(scanner.hasNextLine()) {
+              String line = scanner.nextLine();
+              String[] parts = line.split(" "); //Split by space
+              if (parts.length != 4) {
+                  //Skip invalid line.
+                  continue;
+              }
+              
+              int id = Integer.parseInt(parts[0]);
+              String name = parts[1] + " " + parts[2];
+              double grade = Double.parseDouble(parts[3]);
+              
+              //Make new Student object and append to list.
+              studentList.add(new Student(id, name, grade));
+			}
+			//Assign students array to array from studentList.
+			students = studentList.toArray(new Student[0]);
+			
+			scanner.close();
+			input.close();
+		}
+		
+		catch(Exception e) {
+	        e.printStackTrace();
+	        System.out.println("Cannot read/find the file or initialize objects.");
+	        return false;
+	     }
+		
 		return true;
 	}
 	
